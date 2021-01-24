ca5e349c8a97f31072f52ec2a53b618b4de9f599
csuzum <csuzumura3@gmail.com>
2021-01-23 16:27:45 -0800

    add variables & operators

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -1,19 +1,18 @@
-//operators: = + * - / % ++ -- **
+// Lesson 02-01: 4 variables, using operators: =  +  *  \  -  /  %  ++  --  **
 
 let cheesePizza = 17;
 let pepperoni = 3;
-let mushrooms = 2;
 let olives = 2;
 let coupon = -1.5;
 
-console.log(cheesePizza * 2 + pepperoni + mushrooms * 2);
+console.log(cheesePizza * 2 + pepperoni + olives * 2);
 
-console.log( ((cheesePizza + pepperoni) * 2) + (cheesePizza + mushrooms + olives / 2) * 3);
+console.log( ((cheesePizza + pepperoni) * 2) + (cheesePizza + olives + olives / 2) * 3);
 
 console.log((cheesePizza + olives / 2) + coupon);
 
 console.log(cheesePizza % coupon);
 
-console.log((cheesePizza + ++pepperoni) + (cheesePizza + --mushrooms));
+console.log((cheesePizza + ++pepperoni) + (cheesePizza + --olives));
 
 console.log(cheesePizza ** 2);
