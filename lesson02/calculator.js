386ebce9e3008d24ad07b6f64da62f7db8cc02de
csuzum <csuzumura3@gmail.com>
2021-01-26 22:09:36 -0800

    use if else

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -1,18 +1,15 @@
-// Lesson 02-01: 4 variables, using operators: =  +  *  \  -  /  %  ++  --  **
-
+// Lesson 02-01: 4 variables, using operators
 let cheesePizza = 17;
 let pepperoni = 3;
 let olives = 2;
 let coupon = -1.5;
 
-console.log(cheesePizza * 2 + pepperoni + olives * 2);
 
-console.log( ((cheesePizza + pepperoni) * 2) + (cheesePizza + olives + olives / 2) * 3);
+console.log((cheesePizza + ++pepperoni) * (cheesePizza + --olives) + coupon);
 
-console.log((cheesePizza + olives / 2) + coupon);
 
-console.log(cheesePizza % coupon);
-
-console.log((cheesePizza + ++pepperoni) + (cheesePizza + --olives));
-
-console.log(cheesePizza ** 2);
+if ((cheesePizza + olives + pepperoni) * 2 >= 30) {
+   console.log((cheesePizza + olives + pepperoni) * 2 + coupon);
+}  else {
+   console.log((cheesePizza + olives + pepperoni) * 2);
+}
