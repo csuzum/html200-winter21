5e026642dc2e6769865a4e60ed75bc817f6579f3
csuzum <csuzumura3@gmail.com>
2021-01-26 23:54:27 -0800

    initial commit

---
+++ lesson02/canDrive.js
@@ -0,0 +1,14 @@
+let firstName = 'Cindy';
+let lastName = 'Suzumura';
+let age = 54;
+let drivingAge = 16;
+
+
+if (age > drivingAge) {
+  console.log(firstName + ' ' + lastName + ' ' + 'Can drive. Has driven for' + ' ' + (age - drivingAge) + ' ' + 'years.');
+}
+else {
+  for (let i = 5; i >= 0; i--) {
+  console.log(i);
+  }
+}
