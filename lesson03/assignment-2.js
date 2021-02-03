0f381a728690249ddc5f0ebce33a019510fa811e
csuzum <>
2021-02-02 16:44:03 -0800

    added code to remove period from string

--- lesson03/assignment-2.js
+++ lesson03/assignment-2.js
@@ -1,6 +1,9 @@
 // Set a string variable to be a sentence of at least five words and a period
 let str = 'I want sake and amaiebi.';
 
-// make array from sentence string and console log 3rd item
-let arr = str.trim().split(" ");
+// remove period and make array from sentence string
+let arr = str.slice(0, 23).trim().split(" ");
+  console.log(arr);
+
+// console log the 3rd word
   console.log(arr[2]);
