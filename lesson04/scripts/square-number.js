bf367c92c9fc0682bec87979465b372ba8d6e7aa
csuzum <>
2021-02-09 14:19:56 -0800

    initial commit

---
+++ lesson04/scripts/square-number.js
@@ -0,0 +1,7 @@
+let input = prompt('Enter a number');
+
+function squareNumber(x) {
+  x = x * x;
+  alert(x);
+}
+squareNumber(input);
---
+++ lesson04/scripts/two-numbers.js
@@ -0,0 +1,9 @@
+let input1 = prompt('Enter a number');
+let input2 = prompt('Enter a second number');
+
+function multiply(input1, input2) {
+  total = input1 * input2;
+
+  alert(total);
+}
+multiply(input1, input2);
